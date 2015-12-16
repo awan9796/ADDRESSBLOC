9ea31fbe49b33934f02decf61609c837acdcc069
Ada Wan <awan9796@gmail.com>
2015-12-15 21:17:42 -0500

    CP 19_assignment commit

--- models/address_book.rb
+++ models/address_book.rb
@@ -8,6 +8,18 @@ require_relative 'entry'
      @entries = []
    end
 
+   def remove_entry(name, phone_number, email)
+     delete_entry = nil
+
+     @entries.each do |entry|
+       if name == entry.name && phone_number == entry.phone_number && email == entry.email
+         delete_entry = entry
+       end
+     end
+
+     @entries.delete(delete_entry)
+
+   end
 
    def add_entry(name, phone_number, email)
 
@@ -19,7 +31,7 @@ require_relative 'entry'
        end
        index += 1
      end
- 
+
      @entries.insert(index, Entry.new(name, phone_number, email))
    end
  end
