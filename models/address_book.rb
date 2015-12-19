35a11f1687524bf827f0f73b0967482adec8ccb7
Ada Wan <awan9796@gmail.com>
2015-12-18 23:07:34 -0500

    CP22 Commit

--- models/address_book.rb
+++ models/address_book.rb
@@ -29,4 +29,25 @@ require_relative 'entry'
      end
 
    end
+
+   def binary_search(name)
+     lower = 0
+     upper = @entries.length - 1
+
+     while lower <= upper
+
+       mid = (lower + upper) /2
+       mid_name = @entries[mid].name
+
+       if name == mid_name
+         return @entries[mid]
+       elsif name < mid_name
+         upper = mid -1
+       elsif name > mid_name
+         lower = mid + 1
+       end
+
+   end
+ end
+
  end
